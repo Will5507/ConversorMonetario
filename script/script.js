6838428627e709e3a803ddd341ffdf52ce9d3216
William Andrey Silva <46973365+Will5507@users.noreply.github.com>
2022-08-10 22:46:40 -0300

    trabalhando responsividade

--- script/script.js
+++ script/script.js
@@ -1,47 +1,47 @@
-// as moedas terão um valor padrão dentro do javascript
-// quero pegar o valor do input "valor"
-//envia danado
-
-const calcular = document.querySelector('#calcular')
+const btn = document.querySelector('#calcular') // botao 
+const moedaInicial = document.querySelector('#ConverInicial')// input pra conversao inicial
+const valor = document.querySelector('#valor')// input do valor a ser convertido
+const recebeValor =  valor.value //recebe valor do input digitado pelo usuario
 var conversao = 0
-let ConversorInicial = 0
-let ConversorFinal = 0
 
 
 
 
-calcular.addEventListener('click', function (e){
+btn.addEventListener('click', function() {
 
-    e.preventDefault()
 
-    const moedas = document.querySelectorAll('.recebeMoedas')
 
-    const valor = document.querySelector('#valor')
-    const recebeValor =  valor.value
 
-    for(let i of moedas) {
+
+        if (moedaInicial  == 'Dolar (USD)') {
+
+                conversao = recebeValor * 0.19
+                
+        }if(moedaInicial == "Real (BRL)") {
+
+                conversao = recebeValor * 5.25
+
+
+        }
+
         
-        if(i.value === 'Dolar (USD)') {
-            conversao = recebeValor *  5.15 // Cotação diária
-        } if(i.value === 'Real (BRL)') {
-            conversao = recebeValor * 0.19
-        } 
-    }
-
-     // criar uma div html via js com o valor de conversao
-    //const body = document.querySelector('body')
-    //const corpo1 = document.querySelector('.corpo1')
-    //const form = document.querySelector('form')
-
-    const third = document.querySelector('.third')
-    const div1 = document.createElement('div')
-    div1.classList.add('resultado')
-    div1.innerHTML = conversao
-  
-    third.appendChild(div1)
-    
-
-   // console.log(recebeValor)  // na verdade quero adicionar esse valor como um elemento HTML novo
+        const div1 = document.createElement('div')    
+        div1.classList.add('resultado')
+        div1.innerHTML = conversao
+        btn.appendChild(div1)
 
 })
+
+
+
+
+
+
+
+    
+
+  
+
+
+
 
